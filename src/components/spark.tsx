4d9033927610e0472ac1936a4420da771d20f46f
solscooldude <solaia@atamas.com>
2026-09-15 22:13:28 +0200

    Restore spark.tsx header only - INCOMPLETE do not leave

--- src/components/spark.tsx
+++ src/components/spark.tsx
@@ -1,1 +1,36 @@
-PLACEHOLDER
+"use client";
+
+import { useEffect, useId, useRef, useState, type Ref } from "react";
+import "@/app/sprite-motion.css";
+import {
+  getSparkAura,
+  getSparkTint,
+  type SparkAuraId,
+  type SparkGearId,
+  type SparkTintId,
+  type SparkTrailId,
+} from "@/lib/appearance";
+import { SparkParticleRing, SubjectFlourish } from "@/components/spark-flourishes";
+import { SPARK_BODY_PATH } from "@/lib/spark-silhouette";
+import {
+  SPARK_FLAVOR_INK,
+  sparkFlavorFromContext,
+  sparkHintFromTask,
+  type SparkFlavor,
+} from "@/lib/spark-flavor";
+import { TASK_SUBJECT, type SubjectId, type TaskId } from "@/lib/constants";
+import { type SnackId, type SparkAct } from "@/lib/spark-play";
+import { displaySpriteName } from "@/lib/sprite-name";
+import { sparkEvolutionFromState } from "@/lib/stats";
+import { useCatalyst } from "@/lib/store";
+import { cn } from "@/lib/utils";
+
+export type SparkMood =
+  | "idle"
+  | "locked"
+  | "earning"
+  | "done"
+  | "tempted"
+  | "annoyed"
+  | "sleepy"
+  | "eating";
